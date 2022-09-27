c52310b8b79840786c1fa9c7b74b5ab6ffefb042
ZSamuels28 <ZSamuels28@gmail.com>
2022-09-27 15:50:24 -0700

    code updates

--- ResourceSpace/lambda_function.py
+++ ResourceSpace/lambda_function.py
@@ -1,58 +1,50 @@
-import requests, hashlib, json, os
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+import requests
+import hashlib
+import json
+import os
+import urllib
 
-def getConfig():
-    cfg = {
-        "base_url": os.getenv("BASE_URL", ""),
-        "private_key": os.getenv("PRIVATE_KEY", ""),
-        "user": os.getenv("USER", ""),
-    }
-    return cfg
 
-def signString(query_string):
-    cfg = getConfig()
-    return hashlib.sha256(
-        (cfg.get("private_key") + query_string).encode("utf-8")
-    ).hexdigest()
+def buildQuery(**params):
+    queryString = urllib.parse.urlencode(params, doseq=False)
+    params.update(
+        {
+            "sign": hashlib.sha256(
+                (os.getenv("PRIVATE_KEY") + queryString).encode("utf-8")
+            ).hexdigest()
+        }
+    )
+    return params
 
 
 def lambda_handler(event, context):
-    cfg = getConfig()
 
     if event.get("path") == "/get-resource":
         resourceArray = []
         referenceID = event.get("queryStringParameters").get("referenceID")
-        query = (
-            "user="
-            + cfg.get("user")
-            + "&function=get_resource_all_image_sizes&resource="
-            + referenceID
+        params = buildQuery(
+            **{
+                "user": os.getenv("USER"),
+                "function": "get_resource_all_image_sizes",
+                "resource": referenceID,
+            }
         )
-        response = requests.get(
-            cfg.get("base_url")
-            + "/api/index.php/?"
-            + query
-            + "&sign="
-            + signString(query)
-        )
+        response = requests.get(os.getenv("BASE_URL"), params=params)
 
         for i in response.json():
             if i.get("size_code") == "original":
                 resource = {"referenceID": referenceID, "url": i.get("url")}
-
-                query = (
-                    "user="
-                    + cfg.get("user")
-                    + "&function=get_data_by_field&field=8&ref="
-                    + referenceID
+                params = buildQuery(
+                    **{
+                        "user": os.getenv("USER"),
+                        "function": "get_data_by_field",
+                        "field": "8",
+                        "ref": referenceID,
+                    }
                 )
-                response = requests.get(
-                    cfg.get("base_url")
-                    + "/api/index.php/?"
-                    + query
-                    + "&sign="
-                    + signString(query)
-                )
-
+                response = requests.get(os.getenv("BASE_URL"), params=params)
                 resource.update({"name": response.json(), "path": event.get("path")})
                 resourceArray.append(resource)
 
@@ -61,17 +53,17 @@ def lambda_handler(event, context):
             "headers": {"Content-Type": "application/json"},
             "body": json.dumps({"body": resourceArray}),
         }
-
     elif event.get("path") == "/get-all-resources":
+
         resourceArray = []
-        query = "user=" + cfg.get("user") + "&function=search_get_previews&search=*"
-        response = requests.get(
-            cfg.get("base_url")
-            + "/api/index.php/?"
-            + query
-            + "&sign="
-            + signString(query)
+        params = buildQuery(
+            **{
+                "user": os.getenv("USER"),
+                "function": "search_get_previews",
+                "search": "*",
+            }
         )
+        response = requests.get(os.getenv("BASE_URL"), params=params)
 
         for i in response.json():
             resource = {"name": i.get("field8"), "reference": i.get("ref")}
@@ -82,5 +74,4 @@ def lambda_handler(event, context):
             "headers": {"Content-Type": "application/json"},
             "body": json.dumps({"body": resourceArray}),
         }
-
     return {"statusCode": 404}
